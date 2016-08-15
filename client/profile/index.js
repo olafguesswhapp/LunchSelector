b169b224f9663f9759061365757c34f5d8c6641d
Olaf Peters <olaf@guesswhapp.de>
2016-08-15 11:24:04 +0200

    Show the suppliers the user is reponsible for in /profile

--- client/profile/index.js
+++ client/profile/index.js
@@ -4,25 +4,34 @@ var express = require('express');
 var router = express.Router();
 var Suppliers = require('../../models/suppliers');
 var authentication = require('../../lib/authentication');
-
+var preferredSuppliers;
 router.get('/', authentication.isLoggedIn, function (req, res) {
 		console.log('*** client/profile/index.js');
-		var preferredSuppliers;
 		Suppliers.find({_id: { $in: req.user.preferredSuppliers }}, function(err, suppliers){
 			if (err || suppliers.length === 0){
 				console.log('No suppliers found');
 			} else {
 				preferredSuppliers = suppliers.map(function(supplier){return supplier.supplierName})
 			}
-			var context = {
-			username: req.user.username,
-			name: req.user.name,
-			role: req.user.role,
-			selectedCity: req.user.selectedCity,
-			preferredSuppliers: preferredSuppliers
-		};
-		console.log(context);
-    res.render('../client/profile/profile', context);
+		}).then(function(){
+			Suppliers.find({ _id: { $in: req.user.supplier }})
+							.select( 'supplierName')
+							.exec(function(err, suppliers){
+				if (err){
+					console.log('This user is not in charge of any supplier');
+				} else {
+					var context = {
+						username: req.user.username,
+						name: req.user.name,
+						role: req.user.role,
+						selectedCity: req.user.selectedCity,
+						preferredSuppliers: preferredSuppliers,
+						supplier: suppliers.map(function(supplier){return supplier.supplierName})
+					};
+					console.log(context);
+			    res.render('../client/profile/profile', context);
+				}
+			});
 		});
 });
 
