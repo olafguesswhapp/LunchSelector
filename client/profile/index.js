ed15bb85599a9449c78de9294bfc7a8259e8ebbf
Olaf Peters <olaf@guesswhapp.de>
2016-08-15 09:44:13 +0200

    Display preferred Suppliers on from database in /profile

--- client/profile/index.js
+++ client/profile/index.js
@@ -2,17 +2,28 @@
 
 var express = require('express');
 var router = express.Router();
+var Suppliers = require('../../models/suppliers');
 var authentication = require('../../lib/authentication');
 
 router.get('/', authentication.isLoggedIn, function (req, res) {
 		console.log('*** client/profile/index.js');
-		var context = {
+		var preferredSuppliers;
+		Suppliers.find({_id: { $in: req.user.preferredSuppliers }}, function(err, suppliers){
+			if (err || suppliers.length === 0){
+				console.log('No suppliers found');
+			} else {
+				preferredSuppliers = suppliers.map(function(supplier){return supplier.supplierName})
+			}
+			var context = {
 			username: req.user.username,
 			name: req.user.name,
 			role: req.user.role,
-			selectedCity: req.user.selectedCity
+			selectedCity: req.user.selectedCity,
+			preferredSuppliers: preferredSuppliers
 		};
+		console.log(context);
     res.render('../client/profile/profile', context);
+		});
 });
 
 router.get('/logout', function(req, res){
